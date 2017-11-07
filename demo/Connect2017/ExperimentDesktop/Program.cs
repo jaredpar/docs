b59dccc68bfb0f5a1dcb0d28cf9312d11eadbe65
Jared Parsons <jaredpar@microsoft.com>
2017-11-07 15:51:25 -0800

    Demo

---
+++ demo/Connect2017/ExperimentDesktop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentDesktop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Span<int> s = new [] { 1, 2, 3 };
+            object o = s;
+            Console.WriteLine(s.Length);
+        }
+    }
+}
---
+++ demo/Connect2017/ExperimentDesktop/Scratch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentDesktop.Scratch
+{
+    public readonly ref struct Span<T>
+    {
+        private readonly ref T _pointer;
+        private readonly int _length;
+
+        public ref T this[int index] => ...
+    }
+}
